7e0326bbb70935ad0dc5d69c4ff22e32454aa4f5
prasad-psp <prasad.parshram123@gmail.com>
2025-03-01 12:53:16 +0530

    added google sign in

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
 /**
  * Define and configure your auth resource
@@ -6,6 +6,52 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
+    /**
+     * Default sign up/in option email & password
+     */
     email: true,
+    externalProviders: {
+      google: {
+        /**
+         * This values are stored in aws amplify console
+         * We can create different values for different env in console for dev & prod
+         */
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+
+        /** 
+         * You can determine the pieces of data you 
+         * want to retrieve from each external provider when setting them up in the amplify/auth/resource.ts file using scopes.
+         */ 
+        scopes: ['email'],
+
+        /**
+         * Identity provider (IdP) services store user attributes in different formats. 
+         * When using external IdPs with Amazon Cognito user pools,
+         *  attribute mapping allows you to standardize these varying formats into a consistent schema.
+         */
+        attributeMapping: {
+          email: 'email'
+        },
+      },
+      /**
+       * Using this callback url's when google sign this callback methods are called
+       * callback for after user sign in using google sign in provider
+       * signout for after user sign out using google sign in provider
+       * We need handle this in mobile app using deeplink machanism
+       */
+      callbackUrls: ["myapp://callback/"],
+      logoutUrls: ["myapp://signout/"],
+      // domainPrefix: 'myapp-auth'
+    },
   },
+  // domain: {
+  //   hostedUiDomainPrefix: 'myapp-auth', // Replace with your desired prefix
+  // },
+  userAttributes: {
+    email: {
+      required: true,
+      mutable: true
+    }
+  }
 });
